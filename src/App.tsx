cdbf57750b388c15e61a4c489fd60da7f8d4f416
Tais Calisto <taiscalisto97@gmail.com>
2022-09-26 16:43:20 -0400

    Styled toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { ThemeProvider } from 'styled-components'
-
 import GlobalStyle from './globals/style'
 import theme from './globals/theme'
 import HomePage from './page/HomePage'
+import './globals/index.css'
 
 function App() {
   return (
